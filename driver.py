51e5ca70fe9a33a34ff40c8568c51bfeba8c17ce
Nina Sanchez <sanchez.jenina@gmail.com>
2016-05-17 22:11:21 +0800

    Nina's files

---
+++ driver.py
@@ -0,0 +1,45 @@
+import cv2
+from bam import *
+from process import *
+
+num_rows = 1024
+num_cols = 3071
+    
+if __name__ == '__main__':
+    start_time = time.time()
+    images = load_images_from_folder (INPUT_DIR)
+    
+    # True indicates if a random weight matrix will be initialized.
+    b = bam(num_rows, num_cols)
+    label_of_apple = get_last_label (True, 25) # Uncomment for more general usage
+    # label_of_apple = [1, 1, 1, -1, -1, -1, 1, -1, 1, 1, 1, -1, 1, 1, 1, 1, -1, 1, 1, 1, 1, -1, 1, 1, 1, 1, 1, 1, -1, -1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, -1, 1, 1, 1, -1, 1, 1, 1, 1, 1, -1, 1, 1, -1, -1, 1, 1, 1]
+    print "Finished retrieving last label"
+
+    # for image in images:
+        # bp = get_bipolar_vector(image)
+		
+        # convert_to_image(bp, 'img 1')
+		
+        # total_differences = 0
+        # total_distance = 0.0
+        # iter_count = 1
+        # max_iter_count = 10
+        
+        # inputs_to_train = [bp]
+        # outputs_to_train = [label_of_apple]
+        
+        # while True:
+            # print "now at iteration "+str(iter_count)
+            # b.train(inputs_to_train, outputs_to_train)
+            
+            # if iter_count==max_iter_count:
+                # print "--- --- --- --- --- --- --- --- --- --- ---"
+                # print "Max number of iterations reached! Stopping."
+                # print "Check line 14 of driver.py to change."
+                # print "--- --- --- --- --- --- --- --- --- --- ---"
+                # break
+            # else:
+                # iter_count += 1
+            # print ""
+        
+        # print "program execution: "+str(time.time()-start_time)
